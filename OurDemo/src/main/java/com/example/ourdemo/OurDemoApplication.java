fb94ca6ee2d61cb6e2bc596865cb84e9459098bc
林辉 <lin.hui13@iwhalecloud.com>
2022-08-25 16:47:15 +0800

    初始化.

---
+++ OurDemo/src/main/java/com/example/ourdemo/OurDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.ourdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OurDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OurDemoApplication.class, args);
+    }
+
+}
---
+++ OurDemo/src/test/java/com/example/ourdemo/OurDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ourdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OurDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
